e1cb576d2a89cfd6e20324b081a709027b0abf1d
LinhoBeat <lv.mdluli@outlook.com>
2022-10-18 16:51:39 -0700

    Hello earth

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ *Description: main- Printsout a string to stdout
+ *Return: to 0 if success
+ *
+ **/
+ int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
