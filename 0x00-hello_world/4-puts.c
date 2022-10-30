d723b490eba039fe504707e6b2f998644964ea6f
LinhoBeat <lv.mdluli@outlook.com>
2022-10-30 05:26:50 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 /**
- * Description: main- Printsout a string to stdout
  *
- * Return: to 0 if success
- */
-int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
+ *  * Description: main- Printsout a string to stdout
+ *
+ *   * Return: to 0 if success
+ *
+ *    */
 
-	return (0);
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
